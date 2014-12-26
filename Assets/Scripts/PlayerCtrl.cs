cf5e5ecb6f7ade5323f86106626bee69d92460f5
chocopun <b1012181@Project20-no-MacBook-Air-2.local>
2014-12-26 23:17:16 +0900

    first commit

---
+++ Assets/Scripts/PlayerCtrl.cs
@@ -0,0 +1,144 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerCtrl : MonoBehaviour {
+	const float RayCastMaxDistance = 100.0f;
+	CharacterStatus status;
+	CharaAnimation charaAnimation;
+	Transform attackTarget;
+	InputManager inputManager;
+	public float attackRange = 1.5f;
+	GameObject enemyAttackTarget;
+	GameObject enemyBase;
+	EnemyCtrl enemyCtrl;
+	
+	// ステートの種類.
+	enum State {
+		Walking,
+		Attacking,
+		Died,
+	} ;
+	
+	State state = State.Walking;		// 現在のステート.
+	State nextState = State.Walking;	// 次のステート.
+	
+	
+	// Use this for initialization
+	void Start () {
+		status = GetComponent<CharacterStatus>();
+		charaAnimation = GetComponent<CharaAnimation>();
+		inputManager = FindObjectOfType<InputManager>();
+		enemyBase = GameObject.Find("EnemyBase");
+		enemyCtrl = GetComponent<EnemyCtrl>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		switch (state) {
+		case State.Walking:
+			Walking();
+			break;
+		case State.Attacking:
+			Attacking();
+			break;
+		}
+		
+		if (state != nextState)
+		{
+			state = nextState;
+			switch (state) {
+			case State.Walking:
+				WalkStart();
+				break;
+			case State.Attacking:
+				AttackStart();
+				break;
+			case State.Died:
+				Died();
+				break;
+			}
+		}
+	}
+	
+	
+	// ステートを変更する.
+	void ChangeState(State nextState)
+	{
+		this.nextState = nextState;
+	}
+	
+	void WalkStart()
+	{
+		StateStartCommon();
+	}
+	
+	void Walking()
+	{
+		if (attackTarget) {
+			Debug.Log(attackTarget);
+			Vector3 hitPoint = attackTarget.position;
+			hitPoint.y = transform.position.y;
+			float distance = Vector3.Distance(hitPoint,transform.position);
+			Debug.Log(distance);
+			if (distance < attackRange) {
+				ChangeState(State.Attacking);
+			} else {
+				SendMessage("SetDestination",attackTarget.position);
+			}
+		} else {
+			SendMessage("SetDestination", enemyBase.transform.position);
+		}
+	}
+	
+	// 攻撃ステートが始まる前に呼び出される.
+	void AttackStart()
+	{
+		StateStartCommon();
+		status.attacking = true;
+		
+		// 敵の方向に振り向かせる.
+		Vector3 targetDirection = (attackTarget.position-transform.position).normalized;
+		SendMessage("SetDirection",targetDirection);
+		
+		// 移動を止める.
+		SendMessage("StopMove");
+	}
+	
+	// 攻撃中の処理.
+	void Attacking()
+	{
+		if (charaAnimation.IsAttacked())
+			ChangeState(State.Walking);
+	}
+	
+	void Died()
+	{
+		status.died = true;
+		if(SpawnManager.existPlayers == null){
+			enemyCtrl.SendMessage("SetAttackTarget", null);
+		}
+			enemyCtrl.SendMessage("SetAttackTarget", SpawnManager.existPlayers[0].transform);
+	}
+	
+	void Damage(AttackArea.AttackInfo attackInfo)
+	{
+		status.HP -= attackInfo.attackPower;
+		if (status.HP <= 0) {
+			status.HP = 0;
+			// 体力０なので死亡ステートへ.
+			ChangeState(State.Died);
+		}
+	}
+	
+	// ステートが始まる前にステータスを初期化する.
+	void StateStartCommon()
+	{
+		status.attacking = false;
+		status.died = false;
+	}
+
+    public void SetAttackTarget(Transform target)
+    {
+        attackTarget = target;
+    }
+}
---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SpawnManager : MonoBehaviour {
+    public GameObject playerPrefab;
+    public GameObject wargPrefab;
+    public GameObject dragonPrefab;
+    public Transform playerSpawnPoint;
+    public Transform enemySpawnPoint;
+    public static List<GameObject> existPlayers;
+    public static List<GameObject> existEnemys;
+    GameObject player, warg;
+    GameObject playerBase, enemyBase;
+    PlayerCtrl playerCtrl;
+    EnemyCtrl enemyCtrl;
+
+    void Start () {
+        existPlayers = new List<GameObject>();
+        existEnemys = new List<GameObject>();
+        playerBase = GameObject.Find("PlayerBase");
+        enemyBase = GameObject.Find("EnemyBase");
+        playerCtrl = GetComponent<PlayerCtrl>();
+        enemyCtrl = GetComponent<EnemyCtrl>();
+    }
+
+    void SpawnPlayer () {
+        existPlayers.Add(Instantiate(playerPrefab,playerSpawnPoint.position,playerSpawnPoint.rotation) as GameObject);
+        //player = Instantiate(playerPrefab,playerSpawnPoint.position,playerSpawnPoint.rotation) as GameObject;
+    }
+
+    void SpawnWarg () {
+        existEnemys.Add(Instantiate(wargPrefab,enemySpawnPoint.position,enemySpawnPoint.rotation) as GameObject);
+        //warg = Instantiate(wargPrefab,enemySpawnPoint.position,enemySpawnPoint.rotation) as GameObject;
+    }
+
+    void SpawnDragon () {
+        existEnemys.Add(Instantiate(dragonPrefab,enemySpawnPoint.position,enemySpawnPoint.rotation) as GameObject);
+    }
+
+    public static GameObject PlayerAttackTarget() {
+        return existPlayers[0];
+    }
+
+    public static GameObject EnemyAttackTarget() {
+        return existEnemys[0];
+    }
+}
